2a4563d1b3c12aa3541c5ce8632bb6b7e8a4b088
bossSurface <surapong.kawkangploo@gmail.com>
2018-06-26 10:25:33 +0700

    add css bloomer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as  from 'bloomer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Card>
+                <CardHeader>
+                    <CardHeaderTitle>
+                        Component
+        </CardHeaderTitle>
+                    <CardHeaderIcon>
+                        <Icon className="fa fa-angle-down" />
+                    </CardHeaderIcon>
+                </CardHeader>
+                <CardImage>
+                    <Image isRatio='4:3' src='https://via.placeholder.com/1280x960' />
+                </CardImage> 
+                <CardContent>
+                    <Media>
+                        <MediaLeft>
+                            <Image isSize='48x48' src='https://via.placeholder.com/96x96' />
+                        </MediaLeft>
+                        <MediaContent>
+                            <Title isSize={4}>John Wick</Title>
+                            <Subtitle isSize={6}>@John Wick</Subtitle>
+                        </MediaContent>
+                    </Media>
+                    <Content>
+                        People Keep Asking If I’m Back, And I Haven’t Really Had An Answer, But Now, Yeah, I’m Thinking I’m Back.
+            <br />
+                        <small>11:09 PM - 30 October 2014</small>
+                    </Content>
+                </CardContent>
+            </Card> 
+        );
+    }
 }
 
 export default App;
