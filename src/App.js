b786f0807fb6f929d6937b1b42eb9ae73e5290fb
ibossJ <surapong.kawkangploo@gmail.com>
2018-06-26 11:18:05 +0700

    add bluma css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import * as  from 'bloomer';
+
 
 class App extends Component {
     render() {
         return (
-            <Card>
-                <CardHeader>
-                    <CardHeaderTitle>
-                        Component
-        </CardHeaderTitle>
-                    <CardHeaderIcon>
-                        <Icon className="fa fa-angle-down" />
-                    </CardHeaderIcon>
-                </CardHeader>
-                <CardImage>
-                    <Image isRatio='4:3' src='https://via.placeholder.com/1280x960' />
-                </CardImage> 
-                <CardContent>
-                    <Media>
-                        <MediaLeft>
-                            <Image isSize='48x48' src='https://via.placeholder.com/96x96' />
-                        </MediaLeft>
-                        <MediaContent>
-                            <Title isSize={4}>John Wick</Title>
-                            <Subtitle isSize={6}>@John Wick</Subtitle>
-                        </MediaContent>
-                    </Media>
-                    <Content>
-                        People Keep Asking If I’m Back, And I Haven’t Really Had An Answer, But Now, Yeah, I’m Thinking I’m Back.
-            <br />
-                        <small>11:09 PM - 30 October 2014</small>
-                    </Content>
-                </CardContent>
-            </Card> 
+            <div>
+                hEllo
+            </div>
         );
     }
 }
